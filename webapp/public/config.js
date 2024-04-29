9a20971edc5495f34f531c78d110e7956615ccf3
Jayana Gunaweera <jayana.20200003@iit.ac.lk>
2024-04-29 16:33:25 +0530

    fix:asgardeo sign in

--- webapp/public/config.js
+++ webapp/public/config.js
@@ -1,10 +1,10 @@
 window.config = {
   todoApiUrl:
-    "https://ec149f4e-f145-4064-a8ab-e1fc8f0c563f-dev.e1-us-east-azure.choreoapis.dev/rgyo/todo-svc/graphql-todo-aa9/1.0.0/",
+    "https://8418608d-4932-498b-aeb8-b99c6da0b608-dev.e1-us-east-azure.choreoapis.dev/todoapp/todo/graphql-todo-aa9/v1",
   auth: {
-    signInRedirectURL: "http://localhost:5173",
-    signOutRedirectURL: "http://localhost:5173",
-    clientID: "DXmO3q4E4IAJx9dZE5yyNATRjO4a",
+    signInRedirectURL: "https://d2d9193d-a9cd-474d-84f3-8edc8113a22b.e1-us-east-azure.choreoapps.dev/",
+    signOutRedirectURL: "https://d2d9193d-a9cd-474d-84f3-8edc8113a22b.e1-us-east-azure.choreoapps.dev/",
+    clientID: "oEE0xKDt6jKsN2gqnRnrb2WLYKMa",
     baseUrl: "https://api.asgardeo.io/t/jaycreationz",
   },
 };
